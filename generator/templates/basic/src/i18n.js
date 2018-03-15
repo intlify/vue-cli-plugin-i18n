c930bd5d962037007b32c1bfa7ae59bdf3b1cb05
kazuya kawaguchi <kawakazu80@gmail.com>
2018-03-15 21:04:18 +0900

    :zap: improvement(generator): all locale messages importing

--- generator/templates/basic/src/i18n.js
+++ generator/templates/basic/src/i18n.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import messages from './locales'
 
 Vue.use(VueI18n)
 
+function loadLocaleMessages () {
+  const locales = require.context('./locales', true, /[a-z0-9]+\.json$/i)
+  const messages = {}
+  locales.keys().forEach(key => {
+    const locale = key.match(/([a-z0-9]+)\./i)[1]
+    messages[locale] = locales(key)
+  })
+  return messages
+}
+
 export default new VueI18n({
   locale: 'en',
-  messages
+  fallbackLocale: 'en',
+  messages: loadLocaleMessages()
 })
