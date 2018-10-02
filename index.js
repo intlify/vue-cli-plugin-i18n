c4f8752ef01ff759aa1b96bd4b3768e164ff03e2
kazuya kawaguchi <kawakazu80@gmail.com>
2018-10-02 17:35:34 +0900

    :bug: bug(service): fix cannot import locale messages with 'src' attr at custom blocks
    closes kazupon/vue-i18n-loader#24
    closes kazupon/vue-i18n#401
    closes kazupon/vue-i18n#397

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = (api, options) => {
       webpackConfig.module
         .rule('i18n')
           .resourceQuery(/blockType=i18n/)
+          .type('javascript/auto')
           .use('i18n')
             .loader('@kazupon/vue-i18n-loader')
             .end()
