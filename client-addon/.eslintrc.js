d9707a98f54f19c10c9b71bee5d01a6db3093c8f
kazuya kawaguchi <kawakazu80@gmail.com>
2020-04-24 01:43:27 +0900

    migrate to vue-cli v4 (#95)
    * migrate to vue-cli v4
    
    * fix jest config test match supporting for windows
    
    * update test workflow

--- client-addon/.eslintrc.js
+++ client-addon/.eslintrc.js
@@ -12,11 +12,11 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/standard'
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
