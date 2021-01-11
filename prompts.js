169dc6faf963ffcb20f1dea52acc409381294296
kazuya kawaguchi <kawakazu80@gmail.com>
2021-01-12 02:23:38 +0900

    feat: Vue2 latest supporting (#167)
    * feat: Vue2 latest supporting
    
    * fix: test failed

--- prompts.js
+++ prompts.js
@@ -1,24 +1,41 @@
-module.exports = [{
-  type: 'input',
-  name: 'locale',
-  message: 'The locale of project localization.',
-  validate: input => !!input,
-  default: 'en'
-}, {
-  type: 'input',
-  name: 'fallbackLocale',
-  message: 'The fallback locale of project localization.',
-  validate: input => !!input,
-  default: 'en'
-}, {
-  type: 'input',
-  name: 'localeDir',
-  message: 'The directory where store localization messages of project. It\'s stored under `src` directory.',
-  validate: input => !!input,
-  default: 'locales'
-}, {
-  type: 'confirm',
-  name: 'enableInSFC',
-  message: 'Enable locale messages in Single file components ?',
-  default: false
-}]
+const debug = require('debug')('vue-cli-plugin-i18n:prompts')
+
+module.exports = pkg => {
+  const { semver } = require('@vue/cli-shared-utils')
+  const vue = require('vue')
+  debug('vue version', vue.version)
+  const isVue3 = (vue && semver.major(vue.version) === 3)
+
+  const prompts = [
+    {
+      type: 'input',
+      name: 'locale',
+      message: 'The locale of project localization.',
+      validate: input => !!input,
+      default: 'en'
+    }, {
+      type: 'input',
+      name: 'fallbackLocale',
+      message: 'The fallback locale of project localization.',
+      validate: input => !!input,
+      default: 'en'
+    }, {
+      type: 'input',
+      name: 'localeDir',
+      message: 'The directory where store localization messages of project. It\'s stored under `src` directory.',
+      validate: input => !!input,
+      default: 'locales'
+    }
+  ]
+
+  if (!isVue3) {
+    prompts.push({
+      type: 'confirm',
+      name: 'enableInSFC',
+      message: 'Enable locale messages in Single file components ?',
+      default: false
+    })
+  }
+
+  return prompts
+}
