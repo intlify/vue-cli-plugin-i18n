21d5b5689a56e3c9413c49526ca56e36488bfde9
kazuya kawaguchi <kawakazu80@gmail.com>
2021-04-08 11:59:54 +0900

    feat: support vue-i18n v9.1 (#188)

--- client-addon/src/utils.js
+++ client-addon/src/utils.js
@@ -1,16 +1,20 @@
 import { unflatten } from 'flat'
 
-export function isObject (obj) {
+export function isObject(obj) {
   return obj !== null && typeof obj === 'object'
 }
 
-export function hasChildPaths (path, paths) {
-  if (!path) { return false }
+export function hasChildPaths(path, paths) {
+  if (!path) {
+    return false
+  }
 
-  const localeMessages = unflatten(paths.reduce((val, path) => {
-    val[path] = ''
-    return val
-  }, {}))
+  const localeMessages = unflatten(
+    paths.reduce((val, path) => {
+      val[path] = ''
+      return val
+    }, {})
+  )
   const splits = path.split('.')
 
   // TODO: should be more strogly checking ...
