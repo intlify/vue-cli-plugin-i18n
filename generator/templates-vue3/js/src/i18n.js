23386372a6303fe504014ed88a4c204d4fbb5935
PeterAlfredLee <peteralfredlee@gmail.com>
2021-07-26 11:28:26 +0800

    fix: can not load locale messages in vue3

--- generator/templates-vue3/js/src/i18n.js
+++ generator/templates-vue3/js/src/i18n.js
@@ -2,7 +2,7 @@ import { createI18n } from 'vue-i18n'
 
 /**
  * Load locale messages
- * 
+ *
  * The loaded `JSON` locale messages is pre-compiled by `@intlify/vue-i18n-loader`, which is integrated into `vue-cli-plugin-i18n`.
  * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
  */
@@ -13,7 +13,7 @@ function loadLocaleMessages() {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
-      messages[locale] = locales(key)
+      messages[locale] = locales(key).default
     }
   })
   return messages
